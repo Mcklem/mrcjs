f1c8df097d0267e42aa7b98bbe5ef239521ecd3d
mcklem <jesus_2394@hotmail.com>
2018-12-21 22:49:57 +0100

    Updated MRCJS to 1.0.4 see readme

--- examples/api-server-mongo/node_modules/mrcjs/mrc.js
+++ examples/api-server-mongo/node_modules/mrcjs/mrc.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var fs = require("fs"),
-    path = require('path');
+    path = require('path'),
+	glob = require( 'glob' );
 
 var modelsPath,//MODELS PATH
 routesPath,//ROUTES PATH
@@ -15,10 +16,17 @@ app;//EXPRESS INSTANCE REFERENCE
 var api = {};
 api.middleware_modules = {};
 api.middlewares = {};
-api.models = {};
+api.model_modules = {};
 api.controller_modules = {};
 api.controllers = {};
 api.route_modules = {};
+api.settings = {};
+
+var feedback = {};
+feedback.model_modules = {};
+feedback.middleware_modules = {};
+feedback.controller_modules = {};
+feedback.route_modules = {};
 
 var initialize = function(dir, express){
     __root = dir;
@@ -28,98 +36,132 @@ var initialize = function(dir, express){
     controllersPath = path.join(__root, 'api', 'controllers');
 	middlewaresPath =  path.join(__root, 'api', 'middlewares');
     settingsPath =  path.join(__root, 'api', 'settings.json');
-    api.settings = require(settingsPath); 
+    loadSettings();
 }
 
-//Generic data loader, gets all files in all directories
-var loadData = function(dirPath, onFileFound, OnDirectoryFound, OnFileNotFound){
-    fs.readdirSync(dirPath).forEach(function(file) {
-        var filePath = path.join(dirPath, file);
-		if(fs.existsSync(filePath)){
-			if(!fs.lstatSync(filePath).isDirectory()){
-				onFileFound(filePath, file);
-			}
-			else OnDirectoryFound(filePath);
-		}
-		else OnFileNotFound(filePath);
+var loadSettings = function(){ 
+    api.settings = require(settingsPath);
+    if(api.settings.logReportPath==undefined) api.settings.logReportPath = "./api-report.log";
+}
+
+var loadModelModules = function(){ 
+    
+    glob.sync( modelsPath + '/**/*.js' ).forEach( function( file ) {
+        var fileName = path.basename(file, path.extname(file));
+        feedback.model_modules[fileName] = {};
+        feedback.model_modules[fileName].loaded = false;
+        try
+        {       
+            var module = require(path.resolve(file));
+            api.model_modules[fileName] = module;
+            feedback.model_modules[fileName].loaded = true;
+            //console.log("Model module loaded: " + fileName);
+        }
+        catch(err) {
+            feedback.model_modules[fileName].error = err.stack;
+        }
+	});
+}
+
+var loadControllerModules = function(){
+    glob.sync( controllersPath + '/**/*.js' ).forEach( function( file ) {  
+        var fileName = path.basename(file, path.extname(file));
+        feedback.controller_modules[fileName] = {};
+        feedback.controller_modules[fileName].loaded = false;
+        try
+        {
+            //console.log("Controller module found: " + fileName);
+            if(api.settings.api_modules[fileName]===undefined){
+                feedback.controller_modules[fileName].error = "Not found " + fileName + " in settings file";
+                return;
+            }
+            else if(api.settings.api_modules[fileName].enabled==false){ 
+                feedback.controller_modules[fileName].error =  "Controller module " + fileName + " is disabled";
+                return; 
+            }
+            var module = require(path.resolve(file));
+            api.controller_modules[fileName] = module;
+            appendModule(api.controllers, module);
+            //console.log("Controller module loaded: " + fileName);
+            feedback.controller_modules[fileName].loaded = true;
+        }
+        catch(err) {
+            feedback.controller_modules[fileName].error = err.stack;
+        }
+	});
+}
+
+var loadRouteModules = function(){
+    glob.sync( routesPath + '/**/*.js' ).forEach( function( file ) {
+        var fileName = path.basename(file, path.extname(file));
+        feedback.route_modules[fileName] = {};
+        feedback.route_modules[fileName].loaded = false;
+        try
+        {
+            //console.log("Route module found: " + fileName);
+            if(api.settings.api_modules[fileName]===undefined){
+                feedback.route_modules[fileName].error = "Not found " + fileName + " in settings file";
+                return;
+            }
+            else if(api.settings.api_modules[fileName].enabled==false){ 
+                feedback.route_modules[fileName].message = "Route module " + fileName + " is disabled";
+                return;
+            }
+            var module = require(path.resolve(file));
+            api.route_modules[fileName] = module;
+            module(app, api.controller_modules[fileName]);
+            feedback.route_modules[fileName].loaded = true;
+            //console.log("Route module loaded: " + fileName);
+        }
+        catch(err) {
+            feedback.route_modules[fileName].error = err.stack;
+        }
+	});
+    
+    //SAVE ROUTE FEEDBACKS
+    feedback.routes = [];
+    app._router.stack.forEach(function(r){
+      if (r.route && r.route.path){
+        feedback.routes.push(r.route.path);
+      }
     });
-};
+}
 
-var loadModels = function(){
-    loadData(modelsPath,function(filePath, file){
-        var model = require(filePath);
-        api.models[path.basename(filePath, path.extname(filePath))] = model;
-        console.log("Loaded model: " + file);
-    },
-    function(filePath){
-        loadModels(filePath);
-    },
-    function(filePath){
-        console.error("Unable to load " + file + ", file not found: " + filePath);
-    });
-};
-
-var loadControllersAndRouteModules = function(app){
-	
-	for(var module in api.settings.api_modules){
-		
-		var element = api.settings.api_modules[module];
-		
-		console.log("Found api module: " + module + ", enabled: " +  element.enabled);
-        
-		if(!element.enabled) continue;
-
-  		let controllerPath = path.join(controllersPath, module);
-  		let routePath = path.join(routesPath, module);
-
-		try {
-            var controller = require(controllerPath);
-            controller.api = api;
-            api.controller_modules[path.basename(controllerPath, path.extname(controllerPath))] = controller;
-            appendModule(api.controllers, controller);
-            
-			var route = require(routePath);
-            api.route_modules[path.basename(routePath, path.extname(routePath))] = route;
-            
-            //pass argument to route method
-		    route(app, controller);
-		}
-		catch (e) {
-			if (e instanceof Error && e.code === "MODULE_NOT_FOUND")
-				console.error("Can't load " + module + "\n" + e);
-			else
-				throw e;
-		}
-	}
-};
-
-var loadMiddlewareModules = function(app){
-	requireMiddlewares();
-	for(var middlewareId in api.settings.middlewares){
-		
-		var element = api.settings.middlewares[middlewareId];
-			
-		for(var index in element.routes){
-			var route = element.routes[index];
-			app.use(route, api.middlewares[middlewareId]);
-            console.log("Using middleware: " +  middlewareId + " for route " + route);
-		}
-	}
-};
-
-var requireMiddlewares = function(){
-    loadData(middlewaresPath,function(filePath, file){
-        var middleware = require(filePath);
-        api.middleware_modules[path.basename(filePath, path.extname(filePath))] = middleware;
-        appendModule(api.middlewares, middleware);
-        console.log("Loaded middleware module: " + file);
-    },
-    function(filePath){
-        requireMiddlewares(filePath);
-    },
-    function(filePath){
-        console.error("Unable to middleware module " + file + ", file not found: " + filePath);
-    });
+var loadMiddlewareModules = function(){  
+    //Require middleware Modules
+	glob.sync( middlewaresPath + '/**/*.js' ).forEach( function( file ) {
+        var fileName = path.basename(file, path.extname(file));
+        feedback.middleware_modules[fileName] = {};
+        feedback.middleware_modules[fileName].loaded = false;
+        try
+        {
+            var module = require(path.resolve(file));
+            api.middleware_modules[fileName] = module;
+            appendModule(api.middlewares, module);
+            //console.log("Middleware module loaded: " + fileName);
+            feedback.middleware_modules[fileName].loaded = true;
+            //Apply config
+            for(var middlewareId in api.settings.middlewares){
+                
+                var middleware = api.settings.middlewares[middlewareId];
+                if(module[middlewareId]===undefined){
+                    continue;
+                }
+                
+                feedback.middleware_modules[fileName][middlewareId] = {};
+                feedback.middleware_modules[fileName][middlewareId].routes = [];
+                for(var index in middleware.routes){
+                    var route = middleware.routes[index];
+                    app.use(route, api.middlewares[middlewareId]);
+                    feedback.middleware_modules[fileName][middlewareId].routes.push(route);
+                }
+                
+            }
+        }
+        catch(err) {
+            feedback.middleware_modules[fileName].error = err.stack;
+        }
+	});
 };
 
 //Append function from module to list
@@ -137,10 +179,27 @@ function requireFromString(src, filename) {
   return m.exports;
 }*/
 
+var logReport = function(){
+    if(api.settings.logReport){      
+        try{
+            var report = JSON.stringify(feedback, null, 2);
+            fs.writeFile(api.settings.logReportPath, report, function(err) {
+                if(err) {
+                    return console.log(err);
+                }
+            }); 
+        }
+        catch(err) {
+            console.log(err.stack);
+        }
+    }
+}
+
 exports.initialize = function(dir, express){
     initialize(dir, express);
-    loadModels();
-	//Load JS and then read json to apply each exported function to each defined path
-	loadMiddlewareModules(app);
-    loadControllersAndRouteModules(app);
+    loadModelModules();
+	loadMiddlewareModules();
+    loadControllerModules();
+    loadRouteModules();
+    logReport();
 }
